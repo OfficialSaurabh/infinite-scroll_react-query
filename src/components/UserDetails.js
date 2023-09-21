007982f5003ed333d071ef2a4d97ce16a46e9bd0
SAURABH KUMAR <hmsaurabhkr@gmail.com>
2023-09-21 17:07:13 +0530

    add error handling

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,48 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
 import axios from 'axios';
+
+const fetchUser = async (id) => {
+  const response = await axios.get(`https://randomuser.me/api/?seed=${id}`);
+  return response.data.results[0];
+};
+
 const UserDetails = () => {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  // Access the `id` parameter from the route
   const { id } = useParams();
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get(`https://randomuser.me/api/?seed=${id}`);
-        setUser(response.data.results[0]); // Assuming the response structure
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-        setLoading(false);
-      }
-    };
 
-    fetchUser();
-  }, [id]);
-
-  if (loading) {
-    // Handle the case when user is undefined.
-    return <div>Loading user DAETAILS</div>;
-  }
+  const { data: user, isLoading, isError, error } = useQuery(['user', id], () => fetchUser(id), {
+    enabled: !!id,
+  });
 
   return (
     <div className="user-details">
       <h2>User Details</h2>
-      <div>
-        <img src={user.picture?.large} alt={`${user.name?.first} ${user.name?.last}`} />
-      </div>
-      <p>
-        <strong>Name:</strong> {`${user.name?.first} ${user.name?.last}`}
-      </p>
-      <p>
-        <strong>Email:</strong> {user.email}
-      </p>
-      <p>
-        <strong>Date of Birth:</strong> {user.dob?.date}
-      </p>
-      {/* Add more user details as needed */}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : isError ? (
+        <div>Error: {error.message}</div>
+      ) : !user ? (
+        <div>User not found.</div>
+      ) : (
+        <div>
+          <img src={user.picture.large} alt={`${user.name.first} ${user.name.last}`} />
+          <p>
+            <strong>Name:</strong> {`${user.name.first} ${user.name.last}`}
+          </p>
+          <p>
+            <strong>Email:</strong> {user.email}
+          </p>
+          <p>
+            <strong>Date of Birth:</strong> {new Date(user.dob.date).toLocaleDateString()}
+          </p>
+          {/* Add more user details as needed */}
+        </div>
+      )}
     </div>
   );
 };
